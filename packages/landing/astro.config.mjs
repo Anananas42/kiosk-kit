d784919601fe6c4ece9aa4522e65c8857935f3df
Adam Pazderka <pazderka.adam@protonmail.com>
2026-03-19 01:52:36 +0100

    Fix postgres docker port

--- packages/landing/astro.config.mjs
+++ packages/landing/astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 export default defineConfig({
+  site: 'https://kioskk.net',
   integrations: [react()],
   server: { port: 4321 },
 });
