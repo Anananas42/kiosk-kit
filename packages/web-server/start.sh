97b6c41b57d7ab23f07e4ccbd36851be29c6ba26
Adam Pazderka <pazderka.adam@protonmail.com>
2026-03-24 22:09:40 +0100

    Fix start.sh tailscale

--- packages/web-server/start.sh
+++ packages/web-server/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
-tailscale up --authkey="$TAILSCALE_AUTHKEY" --hostname=kioskkit-web
+tailscale up --authkey="$TAILSCALE_AUTHKEY" --advertise-tags=tag:server --hostname=kioskkit-web
 node dist/index.js
