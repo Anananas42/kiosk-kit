386a87d0477dabbef48dc6d270de8b3b1e6b2686
Adam Pazderka <pazderka.adam@protonmail.com>
2026-03-29 15:15:18 +0200

    Fix package imports

--- packages/shared/tsup.config.ts
+++ packages/shared/tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts", "src/pairing.ts"],
-  format: ["esm"],
+  format: ["esm", "cjs"],
   dts: true,
   clean: true,
 });
